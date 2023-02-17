2e4557b8cdc56f86f0bef336d08fadab279e895f
El Ghalbzouri Adnan <alghalbzouri@santeformapro.com>
2023-02-17 22:13:13 +0100

    ajouts du totalLikes && totalPrice

--- scripts/templates/section4Template.js
+++ scripts/templates/section4Template.js
@@ -1,15 +1,21 @@
 class PhotographerSection4 {
   constructor(photographerDataMediaById, photographerDataById) {
     this._photographers = photographerDataMediaById;
+    this.sommesLikes = 0; 
+    
+    for (let i = 0; i < photographerDataMediaById.length; i++) {
+      this.sommesLikes += photographerDataMediaById[i].likes;
+    }
     this._photographers2 = photographerDataById;
   }
-
   createPhotographTotalSection4() {
+
     const photograph_section4 = document.createElement("div");
-    photograph_section4.classList.add("");
+    photograph_section4.classList.add("container-total");
 
     const photographerTotal = `
-          
+          <span>${this.sommesLikes} <i class="heart fas fa-heart"></i></span> 
+          <span>${this._photographers2.price}€ / jours</span>
       `;
 
     photograph_section4.innerHTML = photographerTotal;
