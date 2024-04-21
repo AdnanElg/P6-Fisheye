b0509a895c3aa9b7f2d65d3c3504e079ad77b0e4
El Ghalbzouri Adnan <elghalbzouriadnan@OrdinatabledeEl.lan>
2024-04-21 11:00:35 +0200

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,13 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
